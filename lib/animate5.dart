c23999c418d0866a2d2aa81f80365c968810f1ce
Tao Dong <InMatrix@users.noreply.github.com>
2017-08-20 22:41:36 -0700

    Update simultanous animations

--- lib/animate5.dart
+++ lib/animate5.dart
@@ -2,45 +2,74 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Demonstrate an AnimatedComponent with multiple Tweens
+// Demonstrate an AnimatedWidget with multiple Tweens
 
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
-const kLogoUrl =
-    "https://raw.githubusercontent.com/dart-lang/logos/master/logos_and_wordmarks/dart-logo.png";
-
-class LogoApp extends AnimatedComponent {
+class AnimatedLogo extends AnimatedWidget {
   // We can make the Tweens static because we don't change them.
-  static final _opacityTween = new Tween<double>(begin: 1.0, end: 0.3);
-  static final _dimensionTween = new Tween<double>(begin: 0.0, end: 300.0);
+  static final _opacityTween = new Tween<double>(begin: 1.0, end: 0.1);
+  static final _sizeTween = new Tween<double>(begin: 0.0, end: 300.0);
 
-  LogoApp({Key key, Animation<double> animation})
-      : super(key: key, animation: animation);
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
 
+  @override
   Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
     return new Center(
-        child: new Opacity(
-            opacity: _opacityTween.evaluate(animation),
-            child: new Container(
-                margin: new EdgeInsets.symmetric(vertical: 10.0),
-                height: _dimensionTween.evaluate(animation),
-                width: _dimensionTween.evaluate(animation),
-                child: new Image.network(kLogoUrl))));
+      child: new Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: new Container(
+          margin: new EdgeInsets.symmetric(vertical: 10.0),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: new FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
+
+class LogoApp extends StatefulWidget {
+  @override
+  _LogoAppState createState() => new _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with TickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+
+  initState() {
+    super.initState();
+    controller = new AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = new CurvedAnimation(parent: controller, curve: Curves.easeIn);
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new AnimatedLogo(animation: animation);
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    controller.dispose();
   }
 }
 
 void main() {
-  final AnimationController controller =
-      new AnimationController(duration: const Duration(milliseconds: 2000),
-          vsync: this);
-  final Animation<double> animation =
-      new CurvedAnimation(parent: controller, curve: Curves.bounceIn);
-  animation.addStatusListener((status) {
-    if (status == AnimationStatus.completed) controller.reverse();
-    else if (status == AnimationStatus.dismissed) controller.forward();
-  });
-
-  runApp(new LogoApp(animation: animation));
-  controller.forward();
+  runApp(new LogoApp());
 }
